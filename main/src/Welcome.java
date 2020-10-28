8b6ca35511d31f3b1ab1e013d3158adad2e6e898
Svetlana Albu <svetlana.albu@endava.com>
2020-10-28 20:30:34 +0200

    Change main class from added new Branch2/1

--- main/src/Welcome.java
+++ main/src/Welcome.java
@@ -1,7 +1,9 @@
+
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("2020");
         System.out.println("Moldova");
+        System.out.println("Have a nice day");
     }
 
 }
