8daeed130ea8463eede7d3751430448f4522d42b
Svetlana Albu <svetlana.albu@endava.com>
2020-10-29 11:42:36 +0200

    First commit for Branch3 Last commit for Branch3 Actual commit for Branch3

--- main/src/Welcome.java
+++ main/src/Welcome.java
@@ -3,6 +3,7 @@ public class Welcome {
     public static void main(String[] args) {
         System.out.println("Commit1");
         System.out.println("Commit2");
+        System.out.println("Commit3");
 
     }
 
